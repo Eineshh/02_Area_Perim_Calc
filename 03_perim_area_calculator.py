fbf7b254e9c021c832b9cdb79c5b1cf40e35f8bf
Eineshh <142182572+Eineshh@users.noreply.github.com>
2023-09-01 10:39:40 +1200

    Add files via upload

--- 03_perim_area_calculator.py
+++ 03_perim_area_calculator.py
@@ -27,16 +27,31 @@ def num_check(question):
 
 
 #Main Routine goes here
-width = num_check("Width: ")
-height = num_check("Height: ")
 
-# Calculate area (width x height)
-area = width * height
+# Introduction / Heading print statements
+print()
+print("**** Area Perimeter Calculator ****")
+print()
 
-# Calculate perimeter (width + height) x 2
-perimeter = 2 * (width + height)
+# Start of calculator loop
+keep_going = ""
+while keep_going == "":
 
-# Output area and perimeter
-print("Perimeter: {} units".format(perimeter))
-print("Area: {} square units".format(area))
+    width = num_check("Width: ")
+    height = num_check("Height: ")
 
+    # Calculate area (width x height)
+    area = width * height
+
+    # Calculate perimeter (width + height) x 2
+    perimeter = 2 * (width + height)
+
+    # Output area and perimeter
+    print("Perimeter: {:.2f} units".format(perimeter))
+    print("Area: {:.2f} square units".format(area))
+    print()
+
+    keep_going = input("Press <enter> to keep going or any key to quit")
+
+print()
+print("Thank you for using the area / perimeter calculator")
