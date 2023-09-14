a390b592692f2d1846a190f9caf2206668572e84
Eineshh <142182572+Eineshh@users.noreply.github.com>
2023-09-15 11:03:50 +1200

    Add files via upload

--- 05_fence_calculator.py
+++ 05_fence_calculator.py
@@ -4,106 +4,58 @@
 def num_check(question):
     valid = False
     while not valid:
-
+        
         error = "Please enter a number that is more than zero"
-
+        
         try:
-
+        
             # ask user to enter a number
             response = float(input(question))
-
+            
             # checks number is more than zero
             if response > 0:
                 return response
-
+            
             # outputs error if input is invalid
             else:
                 print(error)
                 print()
-
+                
         except ValueError:
-            print(error)
+            print(error)    
+    
 
-        # Main Routine goes here
-
-
-
+# Main Routine goes here
 
 # Introduction / Heading print statements
 print()
-print("**** Fence Cost Calculator *****")
+print("*** Fence Cost Calculator ***")
 print()
 
 # Start of calculator loop
 keep_going = ""
 while keep_going == "":
-    # call your number checker function three times to get the
-    # width, length and cost_per_m of the fencing
-    print("replace me with input statements")
+    width = num_check("Width: ")
+    length = num_check("Length: ")
+    cost_per_m = num_check("Cost Per Meter: ")
 
-    # Calculate perimeter (width + height) x 2
+    # Calculate Perimeter (length + width) x 2
+    perimeter = 2 * (length + width)
 
-    # Calculate the cost of the fencing (perimeter x price / meter)
+    # Calculate cost of fencing (perimeter x cost/m)
+    cost_per_m = (perimeter * cost_per_m)
 
-    # Output the perimeter and cost of the fencing
+    print()
 
-    keep_going = input("Press <enter> to keep going or any key to quit")
-
-print()
-print("Thanks for using the Fencing cost calculator")
-
-
-# functions go here
-
-# checks input is a number more than zero
-def num_check(question):
-    valid = False
-    while not valid:
-
-        error = "Please enter a number that is more than zero"
-
-        try:
-
-            # ask user to enter a number
-            response = float(input(question))
-
-            # checks number is more than zero
-            if response > 0:
-                return response
-
-            # outputs error if input is invalid
-            else:
-                print(error)
-                print()
-
-        except ValueError:
-            print(error)
-
-        # Main Routine goes here
-
-
-# Introduction / Heading print statements
-print()
-print("**** Fence Cost Calculator *****")
-print()
-
-# Start of calculator loop
-keep_going = ""
-while keep_going == "":
-    # call your number checker function three times to get the
-    # width, length and cost_per_m of the fencing
-    print("replace me with input statements")
-
-    # Calulate perimeter (width + height) x 2
-
-    # Calculate the cost of the fencing (perimeter x price / meter)
-
-    # Output the perimeter and cost of the fencing
+    # Output perimeter and cost of fencing
+    print("The Perimeter is: {:.2f} m".format(perimeter))
+    print("The Cost of This Fencing is: ${:.2f} ".format(cost_per_m))
+    print()
 
     keep_going = input("Press <enter> to keep going or any key to quit")
+    print()
+    print("-" * 30)
+    print()
 
 print()
-print("Thanks for using the Fencing cost calculator")
-
-
-
+print("Thanks for using the fencing calculator")
